15f11ecfb042dde527033ede95a847ff17992cf4
coolrobloxkid23 <carrizalesxavier966@gmail.com>
2025-04-07 20:48:11 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,13 +1,18 @@
--- Server-side script (for the script in GitHub)
-game.ReplicatedStorage.GearRequest.OnServerEvent:Connect(function(player, gearId)
-    -- Check if the player has permission (you can add custom checks here)
-    if player and gearId then
-        local gear = game:GetService("InsertService"):LoadAsset(gearId)
-        
-        for _, item in pairs(gear:GetChildren()) do
-            if item:IsA("Tool") then
-                item.Parent = player.Backpack -- Give gear to the player's inventory
-            end
-        end
-    end
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Remote = ReplicatedStorage:WaitForChild("GearGiver")
+
+Remote.OnServerEvent:Connect(function(player, gearID)
+	if typeof(gearID) == "number" then
+		local success, gear = pcall(function()
+			return game:GetService("InsertService"):LoadAsset(gearID)
+		end)
+		
+		if success and gear then
+			local tool = gear:FindFirstChildOfClass("Tool")
+			if tool then
+				tool.Parent = player.Backpack
+			end
+		end
+	end
 end)
